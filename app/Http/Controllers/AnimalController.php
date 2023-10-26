a3a43f7657d583b33da0e5d152f82d1bb65fd9e5
IlmanNK <ilmanponka345@gmail.com>
2023-10-27 00:13:43 +0700

    Pertemuan 4

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    // property animals0
+    public $animals = [];
+
+    // methode untuk menampilkan data hewan
+    public function index()
+    {
+        echo "menampilkan data animals";
+
+        // loop property animals
+        foreach($this->animals as $animal){
+            echo "- $animal <br>";
+        }
+    }
+
+    // method untuk menambahkan data hewan
+    public function store(Request $request)
+    {
+        echo "Nama hewan: $request->nama";
+        echo "<br>";
+        echo "untuk menambahkan hewan baru";
+
+        // menambahkan data ke property animals
+        array_push($this->animals, $request->animal);
+
+        // memanggil method index
+        $this->index();
+    }
+
+
+    // Method untuk mengedit data hewan
+    public function update(Request $request, $id)
+    {
+        echo "Nama hewan: $request->nama";
+        echo "<br>";
+        echo "untuk mengubah data hewan id $id";
+
+        // update data di property
+        $this->animals[$id] = $request->animal;
+
+        // panggil methode index
+        $this->index();
+    }
+
+
+    // Method untuk menghapus data hewan
+    public function destroy($id)
+    {
+        echo "untuk menghapus hewan ID $id";
+
+        unset($this->animals[$id]);
+        // panggil methode index
+        $this->index();
+    }
+}
